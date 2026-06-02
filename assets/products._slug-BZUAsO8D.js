9324987836ca59abaae7cee6df202be65896af2f
RizwanZafaris <rizwanzaffar.pk@gmail.com>
2026-06-03 03:31:48 +0500

    Static build from HEAD@a68b473
    Auto-generated. Do not edit directly.
    Regenerate with: bun run deploy:git-static

--- assets/products._slug-BZUAsO8D.js
+++ assets/products._slug-BZUAsO8D.js
@@ -1,4 +1,4 @@
-import{D as i,j as t,L as n}from"./index-8KnXTwOR.js";function o(){const{product:e}=i.useLoaderData(),s=e.waitlistEmail?`mailto:${e.waitlistEmail}?subject=${encodeURIComponent(`Waitlist · ${e.name}`)}&body=${encodeURIComponent(`Hi Rizwan,
+import{D as i,j as t,L as n}from"./index-D90qCjGc.js";function o(){const{product:e}=i.useLoaderData(),s=e.waitlistEmail?`mailto:${e.waitlistEmail}?subject=${encodeURIComponent(`Waitlist · ${e.name}`)}&body=${encodeURIComponent(`Hi Rizwan,
 
 Please add me to the ${e.name} preview waitlist.
 
