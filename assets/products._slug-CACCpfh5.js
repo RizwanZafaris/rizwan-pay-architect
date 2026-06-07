d98d781abf11ff6d4d4736148cd4b84397d674f2
RizwanZafaris <rizwanzaffar.pk@gmail.com>
2026-06-07 05:45:08 +0500

    Static build from HEAD@04e57ae
    Auto-generated. Do not edit directly.
    Regenerate with: bun run deploy:git-static

--- assets/products._slug-CACCpfh5.js
+++ assets/products._slug-CACCpfh5.js
@@ -1,4 +1,4 @@
-import{H as i,j as t,L as n}from"./index-CfNDsohb.js";function o(){const{product:e}=i.useLoaderData(),s=e.waitlistEmail?`mailto:${e.waitlistEmail}?subject=${encodeURIComponent(`Waitlist · ${e.name}`)}&body=${encodeURIComponent(`Hi Rizwan,
+import{H as i,j as t,L as n}from"./index-CEJ37gvL.js";function o(){const{product:e}=i.useLoaderData(),s=e.waitlistEmail?`mailto:${e.waitlistEmail}?subject=${encodeURIComponent(`Waitlist · ${e.name}`)}&body=${encodeURIComponent(`Hi Rizwan,
 
 Please add me to the ${e.name} preview waitlist.
 
